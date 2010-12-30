e9e67f2bdb0e0b4a35121c477411e177991cb8cc
Coby Randquist <randquistcp@gmail.com>
2010-12-29 22:05:03 -0800

    tweaked speaker link from schedule page

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -1,7 +1,7 @@
 module PresentationsHelper
   def display_speakers presentation
     presentation.speakers.map {| speaker |
-      link_to(speaker.profile.full_name, speaker_path(speaker))
+      link_to(speaker.profile.full_name, profile_path(speaker.profile))
     }.join(", ")
   end
 
