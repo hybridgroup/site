51487522225c1e3af344de47af6a57f681bb6503
Coby Randquist <randquistcp@gmail.com>
2011-10-30 20:15:49 -0700

    updated isolate and removed modelizer

--- config/isolate.rb
+++ config/isolate.rb
@@ -1,7 +1,6 @@
 gem "rails",         "=2.3.5"
 gem "intercession",  "~> 2.0"      # Session as model
 gem "sqlite3-ruby"                 # database support
-gem "modelizer"
 gem "haml",          "~> 3.0"      # HAML
 gem 'will_paginate', '~> 2.3', :source => 'http://gemcutter.org'
 gem 'rdiscount'
